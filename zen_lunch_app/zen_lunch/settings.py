70be10ecd38e159e28400b8613b7b8a203ac3d5d
Angelica Coleman <acoleman@zendesk.com>
2015-03-05 14:58:02 -0800

    removing secrets

--- zen_lunch_app/zen_lunch/settings.py
+++ zen_lunch_app/zen_lunch/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j@*o-68@36_+ctf9mx&v1gd1+%^_iog=z54npl#ve&84a#qb-z'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
